84d679226f7e29caf25e4bcf08d2c4e2c57f5cba
Andy McAlpin <AndrewMcAlpin@gmail.com>
2022-03-30 19:47:09 -0500

    add half of myPlant routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,11 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const userRoutes = require('./user-routes');
+const userRoutes = require("./user-routes");
+const plantBasicRoutes = require("./plantBasic-routes");
+const myPlantRoutes = require("./myPlant-routes");
 
-router.use('/users', userRoutes);
+router.use("/users", userRoutes);
+router.use("/plants", plantBasicRoutes);
+router.use("/myPlant", myPlantRoutes);
 
 module.exports = router;

52339510531596a3aab16fd4a2ad627b87b8abaf
Jon Taylor <jonnytest1101@icloud.com>
2022-04-04 19:29:30 -0500

    Updates

--- models/props/index.js
+++ models/props/index.js
@@ -4,54 +4,6 @@ const commonOptions = {
   nest: true
 }
 module.exports = {
-  options: {  // Query Options
-     // attributes: attributes.User,
-     // include: {
-     //   ...MyPlant,
-     //   include: {
-     //     ...PlantBasic,
-     //     include: [
-     //       PlantPicture,
-     //       Comment
-     //     ]
-     //   }
-     // }
-     //  ...commonOptions
-    },
-  //   PlantBasicMyPlant: {
-  //     attributes: attributes.PlantBasic,
-  //     include: [
-  //       {
-  //         ...MyPlant,
-  //         include: User
-  //       },
-  //       PlantPicture,
-  //       Comment
-  //     ],
-  //     ...commonOptions
-  //   },
-  //   MyPlant: {
-  //     attributes: attributes.MyPlant,
-  //     include: [
-  //       User,
-  //       {
-  //         ...PlantBasic,
-  //         include: [ PlantPicture, Comment ]
-  //       }
-  //     ],
-  //       ...commonOptions
-  //   },
-  //   PlantPicture: {
-  //     attributes: attributes.PlantPicture,
-  //     include: PlantBasic,
-  //       ...commonOptions
-  //   },
-  //   Comment: {
-  //     attributes: attributes.Comment,
-  //     include: [ User, PlantBasic ],
-  //       ...commonOptions
-  //   }
-  // },
   attributes,
   columns
 }

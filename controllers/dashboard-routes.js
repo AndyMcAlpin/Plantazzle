c8bfe3c9b74db65e5abae041630c38c4cfa126e0
Andy McAlpin <AndrewMcAlpin@gmail.com>
2022-04-05 20:23:25 -0500

    fix ../

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { User, PlantBasic, MyPlant, PlantPicture, PlantGrowing, PlantCare, Comment, Vote } = require('../../models');
-const withAuth = require('../../utils/auth');
+const { User, PlantBasic, MyPlant, PlantPicture, PlantGrowing, PlantCare, Comment, Vote } = require('../models');
+const withAuth = require('../utils/auth');
 
 // get all myPlants for dashboard
 router.get('/', withAuth, (req, res) => {
